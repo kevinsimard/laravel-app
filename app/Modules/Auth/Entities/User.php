c5191820e985383230a1fc6fd2d446d76faa47c5
Kevin Simard <kev.simard@gmail.com>
2016-03-11 10:25:22 -0500

    Add table property to the User entity

--- app/Modules/Auth/Entities/User.php
+++ app/Modules/Auth/Entities/User.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     /**
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
      * @var array
      */
     protected $fillable = [
