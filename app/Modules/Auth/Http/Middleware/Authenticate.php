7a5e2290248055c74186ffc871c4b2bc092f7a57
Kevin Simard <kev.simard@gmail.com>
2016-03-21 21:11:32 -0400

    Throw http exception instead of returning a response

--- app/Modules/Auth/Http/Middleware/Authenticate.php
+++ app/Modules/Auth/Http/Middleware/Authenticate.php
@@ -2,6 +2,8 @@
 
 namespace App\Modules\Auth\Http\Middleware;
 
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
 class Authenticate
 {
     /**
@@ -14,7 +16,7 @@ class Authenticate
     {
         if (\Auth::guard($guard)->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
-                return response('Unauthorized', 401);
+                throw new HttpException(401);
             }
 
             return redirect()->guest('login');
