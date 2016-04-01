79159517294eedc5d647af0d56bea5629fd67df0
Kevin Simard <kev.simard@gmail.com>
2016-03-31 20:05:42 -0400

    Use helper function instead of using Auth facade

--- app/Modules/Auth/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Modules/Auth/Http/Middleware/RedirectIfAuthenticated.php
@@ -12,7 +12,7 @@ class RedirectIfAuthenticated
      */
     public function handle($request, \Closure $next, $guard = null)
     {
-        if (\Auth::guard($guard)->check()) {
+        if (auth()->guard($guard)->check()) {
             return redirect('/');
         }
 
