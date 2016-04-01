1c0fe0929d7df30111532e42db6e4d245322b9b5
Kevin Simard <kev.simard@gmail.com>
2016-03-31 20:03:02 -0400

    Use FormRequest instead of validators in controllers

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 
 abstract class BaseController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs;
 }
