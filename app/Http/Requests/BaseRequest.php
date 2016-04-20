92c81ce84eaadd6e2e43e789819a71e4cd589fdc
Kevin Simard <kev.simard@gmail.com>
2016-04-20 09:24:18 -0400

    Override FormRequest methods

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -2,23 +2,43 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
 
 abstract class BaseRequest extends FormRequest
 {
     /**
-     * @return array
+     * @return bool
      */
-    public function rules()
+    public function authorize(): bool
     {
-        return [];
+        return true;
     }
 
     /**
-     * @return bool
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
      */
-    protected function passesAuthorization()
+    protected function failedValidation(Validator $validator)
     {
-        return true;
+        if ($this->ajax() || $this->wantsJson()) {
+            throw new ValidationException($validator);
+        }
+
+        parent::failedValidation($validator);
+    }
+
+    /**
+     * @return void
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    protected function failedAuthorization()
+    {
+        abort(403);
     }
 }
